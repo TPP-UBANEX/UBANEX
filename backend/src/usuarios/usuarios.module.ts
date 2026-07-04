5cfd7f57a2e6c775afa20305a7332daf5f35f5de
laufrance <lautaro.fran@gmail.com>
2026-07-04 19:37:01 -0300

    fix: login error

--- backend/src/usuarios/usuarios.module.ts
+++ backend/src/usuarios/usuarios.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuariosController } from './usuarios.controller';
 import { UsuariosService } from './usuarios.service';
 import { Usuario } from './usuario.entity';
+import { AuditoriaModule } from '../auditoria/auditoria.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Usuario])],
+  imports: [TypeOrmModule.forFeature([Usuario]), AuditoriaModule],
   controllers: [UsuariosController],
   providers: [UsuariosService],
   exports: [UsuariosService],
