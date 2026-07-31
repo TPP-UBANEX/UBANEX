c0d69bd19e172d0adbab42f09a8efcd43d17df4f
laufrance <lautaro.fran@gmail.com>
2026-07-31 11:37:20 -0300

    feat(participaciones): aceptar campos de perfil en la asignación de director/evaluador

--- backend/src/participaciones-convocatoria/dto/crear-participacion.dto.ts
+++ backend/src/participaciones-convocatoria/dto/crear-participacion.dto.ts
@@ -1,5 +1,8 @@
-import { IsNotEmpty, IsOptional, IsBoolean, IsUUID, IsEnum } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsBoolean, IsUUID, IsEnum, IsString } from 'class-validator';
 import { RolEjecucion } from '../../common/enums/rol-ejecucion.enum';
+import { Genero } from '../../common/enums/genero.enum';
+import { CargoDocente } from '../../common/enums/cargo-docente.enum';
+import { TipoDesignacionDocente } from '../../common/enums/tipo-designacion-docente.enum';
 
 export class CrearParticipacionDto {
   @IsUUID()
@@ -21,4 +24,42 @@ export class CrearParticipacionDto {
   @IsOptional()
   @IsBoolean()
   esDirectorPrincipal?: boolean;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  nombre?: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  apellido?: string;
+
+  @IsOptional()
+  @IsString()
+  telefono?: string;
+
+  @IsOptional()
+  @IsEnum(Genero)
+  genero?: Genero;
+
+  @IsOptional()
+  @IsBoolean()
+  personaConDiscapacidad?: boolean;
+
+  @IsOptional()
+  @IsEnum(CargoDocente)
+  cargoDocente?: CargoDocente;
+
+  @IsOptional()
+  @IsEnum(TipoDesignacionDocente)
+  tipoDesignacionDocente?: TipoDesignacionDocente;
+
+  @IsOptional()
+  @IsString()
+  areaDocente?: string;
+
+  @IsOptional()
+  @IsString()
+  direccionLocalidad?: string;
 }
